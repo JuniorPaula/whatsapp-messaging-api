ba904228e7dcdfaf6501dc3675d774a80a321525
Junior Paula <luke.junnior@icloud.com>
2023-10-19 11:29:34 -0300

    Refactor to move received media message for usecase

--- src/usecases/receivedEvents.js
+++ src/usecases/receivedEvents.js
@@ -1,4 +1,5 @@
-import receivedMidiaMessage from '../utils/received-midia-message.js';
+import ReceivedMediaMessageUsecase from './receivedMediaMessageUsecase.js';
+
 
 export default class ReceivedEvents {
     #sock;
@@ -10,11 +11,20 @@ export default class ReceivedEvents {
     async on() {
         this.#sock.ev.on('messages.upsert', async  ({ messages }) => {
             const m = messages[0]
+            if (!m.message) {
+                return
+            }
+            const messageType = Object.keys(m.message)[0]
+            if (messageType !== 'conversation') {
+                const receivedMediaMessage = new ReceivedMediaMessageUsecase();
+                await receivedMediaMessage.execute(m)
+                return
+            }
+
             /**
-             * Implements logic to handle received messages events
+             * @todo
+             * implementar o caso de mensagem de texto
              */
-            
-            await receivedMidiaMessage(m);
         });
     }
 }
